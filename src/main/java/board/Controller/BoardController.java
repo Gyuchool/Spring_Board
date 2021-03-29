d664fd34bd639d236c7c67120334180ebc06a509
giron <balance0591@naver.com>
2021-03-30 00:02:29 +0900

    세션 추가 예정

--- src/main/java/board/Controller/BoardController.java
+++ src/main/java/board/Controller/BoardController.java
@@ -1,5 +1,6 @@
 package board.Controller;
 
+import board.Domain.Entity.UserEntity;
 import board.dto.BoardDto;
 import board.Service.BoardService;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -70,7 +72,8 @@ public class BoardController {
 
     /* 게시글 삭제 */
     @PostMapping("/post/{no}")
-    public String delete(@PathVariable("no") Long no) {
+    public String delete(@PathVariable("no") Long no, HttpSession session) throws Exception {
+        // == 수정 == //
         boardService.deletePost(no);
         return "redirect:/list";
     }
